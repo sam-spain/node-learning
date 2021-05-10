7e3a88e6e43dcaf55af8f1296f3be88122acf8e5
sam <sam@samspain.com>
2021-05-10 20:27:32 +0100

    Create bootcamp model

---
+++ server/models/Bootcamp.js
@@ -0,0 +1,95 @@
+const mongoose = require("mongoose");
+require("mongoose-type-url");
+const { isEmail, isMobilePhone } = require("validator");
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+    unique: true,
+    trim: true,
+    maxLength: [100, "Name can not be more than 100 characters"],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, "Please add a description"],
+    maxLength: [500, "Description can not be more than 100 characters"],
+  },
+  website: {
+    work: mongoose.SchemaTypes.Url,
+    profile: mongoose.SchemaTypes.Url,
+  },
+  email: {
+    validate: [isEmail, "Invalid email"],
+  },
+  phone: {
+    validate: [isMobilePhone, "Invalid phone number"],
+  },
+  address: {
+    type: String,
+    required: [true, "Please add an address"],
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: "2dsphere",
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  careers: {
+    type: [String],
+    required: true,
+    enum: [
+      "Web Development",
+      "Mobile Development",
+      "UI/UX",
+      "Data Science",
+      "Business",
+      "Other",
+    ],
+  },
+  averageRating: {
+    type: Number,
+    min: [1, "Rating must be at least 1"],
+    max: [10, "Rating can not be more than 10"],
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: "no-photo.jpg",
+  },
+  housing: {
+    type: Boolean,
+    default: false,
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false,
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false,
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
